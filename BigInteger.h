d42a54a2ddef9dc0a84cf9c08b69aa666dee5a04
Abhishek00013 <130584716+Abhishek00013@users.noreply.github.com>
2023-10-25 20:26:28 +0530

    Update BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -1,44 +1,36 @@
-#include<stdio.h>
-#include<stdlib.h>
+#ifndef BIG_INTEGER_H
+#define BIG_INTEGER_H
 
-// Function to insert a new node at the beginning of a linked list
-struct node* insert(struct node* head, int val);
+#include <stdio.h>
+#include <stdlib.h>
 
-// Function to insert a new element at the end of a BigInteger
-void insert_tail(struct BigInteger* num, int element);
+// Define a structure for a node in a linked list
+struct node {
+  int data;
+  struct node *next;
+};
 
-// Function to calculate the length of a linked list
-long int length(struct node* head);
+// Define a structure for a BigInteger
+struct BigInteger {
+  struct node *head;
+  long int length;
+  char sign;
+};
 
-// Function to display a linked list in reverse order
-void displayLL(struct node* link);
-
-// Function to display a BigInteger
+// Function declarations
+struct node *insert(struct node *head, int val);
+void insert_tail(struct BigInteger *num, int element);
+long int length(struct node *head);
+void displayLL(struct node *link);
 void display(struct BigInteger b);
-
-// Function to initialize a BigInteger from a string
-struct BigInteger initialize(char* s);
-
-// Function to reverse a linked list
-struct node* reverse(struct node* head);
-
-// Function to compare two BigIntegers
+struct BigInteger initialize(char *s);
+struct node *reverse(struct node *head);
 int compare(struct BigInteger num1, struct BigInteger num2);
-
-// Function to perform addition of two BigIntegers
 struct BigInteger add(struct BigInteger num1, struct BigInteger num2);
-
-// Function to perform subtraction of two BigIntegers
 struct BigInteger sub(struct BigInteger num1, struct BigInteger num2);
-
-// Function to perform multiplication of two BigIntegers
 struct BigInteger mul(struct BigInteger num1, struct BigInteger num2);
-
-// Function to perform division of two BigIntegers
 struct BigInteger div1(struct BigInteger num1, struct BigInteger num2);
-
-// Function to perform modulus of two BigIntegers
 struct BigInteger mod(struct BigInteger num1, struct BigInteger num2);
+void freeL(struct BigInteger *num);
 
-// Function to free the memory allocated for a BigInteger
-void freeL(struct BigInteger* num);
+#endif // BIG_INTEGER_H
